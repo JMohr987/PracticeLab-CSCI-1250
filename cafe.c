ac74306e22fef0fd4db95955e1791a25a2a849e0
Jackie Mohr <JackieMohr@mac.wifi.etsu.edu>
2025-10-21 14:08:58 -0400

    Full functionality in C

--- cafe.c
+++ cafe.c
@@ -36,9 +36,11 @@ void PrintBanner(char *name, double taxRate){
 
 
 void PrintMenu(char *menu[], int rows){
+    printf("----------------------------------\n");
     for(int i = 0; i < rows; ++i){
         printf("%s\n", menu[i]);
     }
+    printf("----------------------------------\n");
     printf("Make your selection: ");
 }
 
@@ -80,27 +82,127 @@ void GetItemPrice(Item **newItem){
 
 }
 
+void GetItemQuantity(Item **newItem){
+    char temp[MAX_ITEM_LENGTH];
+    
+    printf("Enter item quantity: ");
+
+    while(true){
+        fgets(temp, MAX_ITEM_LENGTH, stdin);
+        temp[strcspn(temp, "\n")] = '\0';
+
+        if(IsNumber(temp)){
+            break;
+        }
+    }
+
+    sscanf(temp, "%d", &((*newItem)->quantity));
+
+}
+
 void AddItem(List *itemList){
     Item *newItem = (Item *)malloc(sizeof(Item));
     GetItemName(&newItem);
     GetItemPrice(&newItem);
+    GetItemQuantity(&newItem);
     itemList->push_back(itemList, newItem);
-    for(Node *it = itemList->head; it != NULL; it = it->nextElement){ 
-        printf("%s: $%.2f\n", 
-            (((Item *)(it->element))->name), (((Item *)(it->element))->price));
-    }
+    //for(Node *it = itemList->head; it != NULL; it = it->nextElement){ 
+    //    printf("%s: $%.2f\n", 
+    //        (((Item *)(it->element))->name), (((Item *)(it->element))->price));
+    //}
 }
 
-    
+void DeleteItem(List *itemList){
+    char temp[MAX_ITEM_LENGTH];
+    int i = 1;
+
+    for(Node *it = itemList->head; it != NULL; it = it->nextElement){
+        printf("%d: %s\n",
+            i,
+            (((Item *)(it->element))->name));
+        ++i;
+    }
+
+    printf("Enter an index to delete an item\n");
+
+    while(true){
+        fgets(temp, MAX_ITEM_LENGTH, stdin);
+        temp[strcspn(temp, "\n")] = '\0';
+
+        if(IsNumber(temp)){
+            break;
+        }
+    }
+    sscanf(temp, "%d", &i);
+    itemList->pop(itemList, i);
+
+}
+
+double ComputeSubtotal(List *itemList){
+    double totalPrice = 0.0;
+
+    for(Node *it = itemList->head; it != NULL; it = it->nextElement){
+        totalPrice += ((Item *)(it->element))->price * ((Item *)(it->element))->quantity;
+    }
+
+    return totalPrice;
+}
+
+double ComputeTax(double subtotal, double rate){
+    return subtotal * rate;
+}
+
+double ApplyDiscount(double subtotal){
+    printf("Enter discount code: ");
+    char temp[MAX_ITEM_LENGTH];
+
+    fgets(temp, MAX_ITEM_LENGTH, stdin);
+    temp[strcspn(temp, "\n")] = '\0';
+
+    if(!strcmp(temp, "STUDENT10")){
+        return subtotal * 0.1;
+    }
+
+    return 0;
+}
+
+void Checkout(List *itemList, double taxRate){
+    double beforeTax = ComputeSubtotal(itemList); 
+    double tax = ComputeTax(beforeTax, taxRate);
+    double savings = ApplyDiscount(beforeTax + tax);
+    double finalTotal = beforeTax + tax - savings;
+
+    printf("----------------------------------\n");
+    printf("----------------------------------\n");
+    printf("RECIEPT\n");
+    for(Node *it = itemList->head; it != NULL; it = it->nextElement){ 
+        printf("Item: %s\n", 
+            (((Item *)(it->element))->name));
+        printf("Quantity: %d\n", 
+            (((Item *)(it->element))->quantity));
+        printf("Unit Price: %.2f\n", 
+            (((Item *)(it->element))->price));
+    }
+    printf("----------------------------------\n");
+    printf("Subtotal: %.2f\n", beforeTax);
+    printf("Tax: %.2f\n", tax);
+    printf("Savings: %.2f\n", savings);
+    printf("Final Total: %.2f\n", finalTotal);
+    printf("Thank you for your purchase!\n");
+    printf("----------------------------------\n");
+    printf("----------------------------------\n");
+}
 
 void ViewCart(List *itemList){
     for(Node *it = itemList->head; it != NULL; it = it->nextElement){ 
-        printf("%s: $%.2f\n", 
-            (((Item *)(it->element))->name), (((Item *)(it->element))->price));
+        printf("%s: $%.2f, x%d\n", 
+            (((Item *)(it->element))->name),
+            (((Item *)(it->element))->price),
+            (((Item *)(it->element))->quantity));
     }
 }
 
-bool MenuSelection(List *itemList){
+bool MenuSelection(List *itemList, double taxRate){
     char selection[6];
     
     while(true){
@@ -135,13 +237,17 @@ bool MenuSelection(List *itemList){
 
     switch(*selection){
         case '1':
-            printf("adding item\n");
             AddItem(itemList);
             break;
         case '2':
-            printf("Viewing Cart!\n");
             ViewCart(itemList);
             break; 
+        case '3':
+            DeleteItem(itemList);
+            break;
+        case '4':
+            Checkout(itemList, taxRate);
+            break;
         case '5':
             printf("Thanks for visiting our cafe!\n");
             return false;
@@ -164,7 +270,7 @@ int main(int argc, char **argv){
     PrintMenu(menu, rows);
 
 
-    while(MenuSelection(&itemList)){
+    while(MenuSelection(&itemList, taxRate)){
         PrintMenu(menu, rows);
     }
 
